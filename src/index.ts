8271a8822abdfa0de8791b6af0eb898b2b8aa1b2
Joel Thorstensson <oed@3box.io>
2020-12-16 12:03:03 +0100

    fix: did parser regex

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import ethereum from './blockchains/ethereum'
 import polkadot from './blockchains/polkadot'
 import eosio from './blockchains/eosio'
 
-const findDID = (did: string): string | undefined => did.match(/(did:(3|muport):[a-zA-Z0-9])\w+/)?.[0]
+const findDID = (did: string): string | undefined => did.match(/(did:[a-zA-Z0-9]+:[a-zA-Z0-9]+)/)?.[0]
 
 const handlers = {
   [ethereum.namespace]: ethereum,
